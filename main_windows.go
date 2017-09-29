5e71bd731c0cee23146f9c1a2ad316a74240ab78
Charlie Vieth <charlie.vieth@gmail.com>
2017-09-28 20:11:26 -0400

    Windows: use sparse file

--- main_windows.go
+++ main_windows.go
@@ -3,11 +3,39 @@ package main
 import (
 	"os"
 	"syscall"
+	"unsafe"
 )
 
+const FSCTL_SET_SPARSE = 590020
+
+type FILE_SET_SPARSE_BUFFER struct {
+	SetSparse uint8
+}
+
 func MakeFile(name string, size int64) (*os.File, error) {
+
 	f, err := os.OpenFile(name, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0644)
 	if err != nil {
+		return nil, err
+	}
+	handle := syscall.Handle(f.Fd())
+
+	// Make file sparse
+
+	buf := FILE_SET_SPARSE_BUFFER{SetSparse: 1}
+	var out uint32
+	err = syscall.DeviceIoControl(
+		handle,                        // hDevice
+		FSCTL_SET_SPARSE,              // dwIoControlCode
+		(*byte)(unsafe.Pointer(&buf)), // lpInBuffer
+		uint32(unsafe.Sizeof(buf)),    // nInBufferSize
+		nil,  // lpOutBuffer
+		0,    // nOutBufferSize
+		&out, // lpBytesReturned
+		nil,  // lpOverlapped
+	)
+	if err != nil {
+		f.Close()
 		return nil, err
 	}
 
@@ -16,7 +44,7 @@ func MakeFile(name string, size int64) (*os.File, error) {
 		return nil, err
 	}
 
-	if err := syscall.SetEndOfFile(syscall.Handle(f.Fd())); err != nil {
+	if err := syscall.SetEndOfFile(handle); err != nil {
 		f.Close()
 		return nil, err
 	}
